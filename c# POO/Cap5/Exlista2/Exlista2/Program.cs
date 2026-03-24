39e17e2d6679588c73f74110a9bcc687e011d143
LucasSouz89 <lucas.ss7@outlook.com>
2026-03-24 11:10:21 -0300

    Fazendo exercicios do cap 5

--- c# POO/Cap5/Exlista2/Exlista2/Program.cs
+++ c# POO/Cap5/Exlista2/Exlista2/Program.cs
@@ -88,6 +88,74 @@ namespace Exlista2 {
                 Console.WriteLine($"Arma #{i+1} {a[i]}");
             }
             */
+            Console.WriteLine("Registrador de zumbis");
+            Console.Write("Digite o numero de zumbis: ");
+            int qtd = int.Parse(Console.ReadLine());
+            List<Zumbi> list = new List<Zumbi>();
+            Console.WriteLine();
+            for (int i = 0; i < qtd; i++) {
+                Console.WriteLine($"Zumbi #{i+1}");
+                Console.Write("Digite o id do zumbi");
+                int id = int.Parse(Console.ReadLine());
+                while (list.Find(x => x.Id == id) != null) { Console.Write("Digite outro id: ");id = int.Parse(Console.ReadLine());}
+                Console.WriteLine("Escolha um tipo de zumbi");
+                Console.WriteLine("1 - Corredor");
+                Console.WriteLine("2 - Tanque");
+                Console.WriteLine("3 - Espreitador");
+                int escolha = int.Parse(Console.ReadLine());
+                string nome = "";
+                bool c = true;
+                while (c) {
+                    switch (escolha) {
+                        case 1: nome = "Corredor";
+                            c = false;
+                            break;
+                        case 2: nome = "Tanque";
+                            c = false;
+                            break;
+                        case 3: nome = "Espreitador";
+                            c = false;
+                            break;
+                        default:
+                            Console.WriteLine("Escollha uma opcão valida: ");
+                            escolha = int.Parse(Console.ReadLine());
+                            break;
+                    }
+                }
+                Console.Write("Digite a vida: ");
+                int vida = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                while (vida <= 0) { Console.Write("Digite a vida valida: "); vida = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture); }
+
+                list.Add(new Zumbi(id,nome,vida));
+                Console.WriteLine();
+            }
+            Console.Write("Qual o ID do zumbi que tomou o tiro? ");
+            int Id = int.Parse(Console.ReadLine());
+            Zumbi zumbiDano = list.Find(x => x.Id == Id);
+            bool condi = true;
+            while (condi) {
+                if (zumbiDano != null) {
+                    Console.Write("Digite o dano aplicado: ");
+                    double dano = double.Parse(Console.ReadLine());
+                    while (dano <= 0) { Console.Write("Digite um dano valido: "); dano = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); }
+                    zumbiDano.TomarDano(dano);
+                    condi = false;
+                } else {
+                    Console.Write("Digite o id valido: ");
+                    Id = int.Parse(Console.ReadLine());
+                    zumbiDano = list.Find(x => x.Id == Id);
+                }
+            }
+            int contador = 1;
+            foreach(Zumbi unidade in list) {
+                if (unidade.Vida == 0) { Console.WriteLine($"Zumbi morto #{contador} {unidade}"); } 
+                else { Console.WriteLine($"Zumbi #{contador} {unidade}"); }
+                contador++;
+            }
+
+            
+
+
         }
 
     }
---
+++ c# POO/Cap5/Exlista2/Exlista2/Zumbi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exlista2 {
+    internal class Zumbi {
+        public int Id { get; private set; }
+        public string Tipo { get; private set; }
+
+        public double Vida { get; private set; }
+
+
+        public Zumbi(int id, string tipo, double vida) {
+            Id = id;
+            Tipo = tipo;
+            Vida = vida;
+        }
+        public void TomarDano(double dano) {
+            Vida -= dano;
+            if (Vida < 0) { Vida = 0; }
+        }
+        public override string ToString() {
+            return $"Tipo: {Tipo}, Vida{Vida.ToString("F2",CultureInfo.InvariantCulture)}, ID:{Id}";
+        }
+    }
+}
