b2dd6c96f4f81c70a4db1e9abcbd8f15da0256af
LucasSouz89 <lucas.ss7@outlook.com>
2026-04-24 10:52:15 -0300

    Fazendo exercicios do cap 11

---
+++ c# POO/Seção 11 - Interfaces/InterfaceIComparable/InterfaceIComparable/Entites/Employer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceIComparable.Entites;
+
+namespace InterfaceIComparable.Entites
+{
+    sealed class Employer : IComparable
+    {
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+
+        public Employer(string csvName)
+        {
+            string[] vet = csvName.Split(',');
+            Name = vet[0];
+            Salary = double.Parse(vet[1],CultureInfo.InvariantCulture);
+        }
+        public override string ToString()
+        {
+            return $"{Name} , Salary $ {Salary.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+        public int CompareTo(Object obj) 
+        {
+            if (!(obj is Employer)) { 
+                throw new ArgumentException("A object is a different class");
+            }
+            Employer other = obj as Employer;
+            return Salary.CompareTo(other.Salary);
+        }
+    }
+}
---
+++ c# POO/Seção 11 - Interfaces/InterfaceIComparable/InterfaceIComparable/Program.cs
@@ -0,0 +1,30 @@
+﻿using InterfaceIComparable.Entites;
+using System.Globalization;
+
+namespace InterfaceIComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string path = @"C:\Temp\in.txt";
+            string path = @"C:\Temp\in2.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employer> list = new List<Employer>();
+                    while (!sr.EndOfStream)
+                    {
+                        
+                        list.Add(new Employer(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (var e in list) { Console.WriteLine(e); }
+
+                }
+            }catch(IOException e) { Console.WriteLine($"An error ocurred: {e.Message}"); }
+        }
+    }
+}
