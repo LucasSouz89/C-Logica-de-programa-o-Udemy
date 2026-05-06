2ee22fe0accaac0a53813851ca1e422c56debf22
LucasSouz89 <lucas.ss7@outlook.com>
2026-05-06 16:16:41 -0300

    Fazendo exercicios do cap 12

--- c# POO/Seção 12 - Generics,Set,Dictionary/DictionaryExercicios/DictionaryExercicios/Program.cs
+++ c# POO/Seção 12 - Generics,Set,Dictionary/DictionaryExercicios/DictionaryExercicios/Program.cs
@@ -50,33 +50,132 @@ namespace DictionaryExercicios
             //{
             //    palavras[letter] = palavras.GetValueOrDefault(letter,0) + 1;
             //}
-            
+
             //foreach (var letter in palavras) {
             //    Console.WriteLine($"{letter.Key} {letter.Value}x");
             //}
             //Ex 5
-            Dictionary<string,string> traducoes = new Dictionary<string,string>();
-            traducoes["cat"] = "gato";
-            traducoes["dog"] = "cachorro";
-            traducoes["pen"] = "caneta";
-            traducoes["black"] = "preto";
-            traducoes["this"] = "esse";
-            traducoes["is"] = "é";
-            Console.Write("Digite um frase: ");
-            string[] frase = Console.ReadLine().ToLower().Split(" ");
-            foreach (string palavra in frase) {
-                if (traducoes.TryGetValue(palavra, out var value))
-                {
-                    Console.Write(value + " ");
-                }
-                else 
-                {
-                    Console.Write(palavra + " ");
-                }
+            //Dictionary<string,string> traducoes = new Dictionary<string,string>();
+            //traducoes["cat"] = "gato";
+            //traducoes["dog"] = "cachorro";
+            //traducoes["pen"] = "caneta";
+            //traducoes["black"] = "preto";
+            //traducoes["this"] = "esse";
+            //traducoes["is"] = "é";
+            //Console.Write("Digite um frase: ");
+            //string[] frase = Console.ReadLine().ToLower().Split(" ");
+            //foreach (string palavra in frase) {
+            //    if (traducoes.TryGetValue(palavra, out var value))
+            //    {
+            //        Console.Write(value + " ");
+            //    }
+            //    else 
+            //    {
+            //        Console.Write(palavra + " ");
+            //    }
+            //}
+            //Ex 6
+            //Dictionary<string, int> filialA = new Dictionary<string, int>() { { "Mouse", 10 }, { "Teclado", 5 } };
+            //Dictionary<string, int> filialB = new Dictionary<string, int>() { { "Monitor", 3 }, { "Mouse", 4 } };
+            //Dictionary<string, int> totalConsolidado = new Dictionary<string, int>();
+
+            //foreach (var item in filialA)
+            //{
+            //    totalConsolidado[item.Key] = totalConsolidado.GetValueOrDefault(item.Key, 0)+ item.Value;
+            //}
+            //foreach (var item in filialB)
+            //{
+            //    totalConsolidado[item.Key] = totalConsolidado.GetValueOrDefault(item.Key, 0) + item.Value; 
+            //}
+            //foreach (var item in totalConsolidado) { Console.WriteLine($"{item.Key} {item.Value} unidades"); }
+            //Ex 7
+            //Dictionary<string,List<double>> alunos = new Dictionary<string,List<double>>();
+            //alunos["Lucas"] = new List<double>() { 8.0, 7.5 , 9.0 };
+            //alunos["Maria"] = new List<double>() { 10.0, 6.0, 8.0 };
+
+            //foreach (var item in alunos) {
+
+            //    double media = item.Value.Average();
+            //    Console.WriteLine($"Media do aluno(a) {item.Key} {media.ToString("F2",CultureInfo.InvariantCulture)}");
+            //}
+            //Ex8
+            //Dictionary<char,List<string>> agrupador = new Dictionary<char,List<string>>();
+            //Console.WriteLine("Digite o nome de cinco animais: ");
+
+            //for (int i = 0; i < 5; i++) {
+            //    string animal = Console.ReadLine().ToLower();
+            //    if (agrupador.TryGetValue(animal[0], out List<string> list)) 
+            //    {
+            //        list.Add(animal);
+
+            //    }
+            //    else 
+            //    {
+
+
+            //        agrupador[animal[0]] = new List<string>() { animal };
+
+            //    }
+            //}
+            //foreach (var i in agrupador)
+            //{
+            //    Console.Write($"Letra {i.Key}: ");
+            //    foreach (var j in i.Value) 
+            //    {
+            //        if (j == i.Value.Last()) { Console.Write(j); }
+            //        else { Console.Write(j + ", "); }
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Ex 9
+            //try
+            //{
+            //    Dictionary<string, int> estoque = new Dictionary<string, int>() { { "Notebook", 5 }, { "Mouse", 2 } };
+            //    Console.Write("Digite o produto a ser comprado e a quantidade(TV 3): ");
+            //    string[] line = Console.ReadLine().Split(" ");
+            //    string name = line[0];
+            //    int qtd = int.Parse(line[1]);
+
+            //    if (estoque.TryGetValue(name, out int value))
+            //    {
+            //        if (value < qtd) { throw new Exception("Quantidade insuficiente"); }
+            //        value -= qtd;
+            //        if (value == 0) { estoque.Remove(name); }
+            //        else
+            //        {
+            //            estoque[name] = value;
+            //        }
+            //    }
+            //    else { { throw new Exception("Produto não encontrado"); } }
+
+            //    foreach (var item in estoque)
+            //    {
+            //        Console.WriteLine($"{item.Key} {item.Value} unidades");
+            //    }
+            //}
+            //catch (Exception ex) { Console.WriteLine(ex.Message); }
+            //Ex 10
+            string[] logServidor = {
+            "amanda 20:45",
+            "alex86 21:49",
+            "amanda 22:10",
+            "bobbrown 23:00",
+            "amanda 23:15",
+            "alex86 23:30"
+             };
+            Dictionary<string,int> logs = new Dictionary<string,int>();
+            foreach (var item in logServidor)
+            {
+                string[] line = item.Split(' ');
+                string name = line[0];
+                if(logs.TryGetValue(name,out int value)) { logs[name] = value + 1; }
+                else { logs[name] = logs.GetValueOrDefault(name,0) + !; }
             }
-
-
-
+            foreach (var item in logs)
+            {
+                Console.WriteLine($"{item.Key} {item.Value} vezes");
+            }
         }
     }
 }
