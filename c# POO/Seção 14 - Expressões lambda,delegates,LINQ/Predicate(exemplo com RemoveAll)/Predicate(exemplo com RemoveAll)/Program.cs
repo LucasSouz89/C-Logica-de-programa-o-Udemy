a222af6cc7ce9c47f06e59363bb08f11c2dd8122
LucasSouz89 <lucas.ss7@outlook.com>
2026-05-12 10:58:50 -0300

    Fazendo exercicios do cap 14

---
+++ c# POO/Seção 14 - Expressões lambda,delegates,LINQ/Predicate(exemplo com RemoveAll)/Predicate(exemplo com RemoveAll)/Program.cs
@@ -0,0 +1,28 @@
+﻿using Predicate_exemplo_com_RemoveAll_.Entites;
+
+namespace Predicate_exemplo_com_RemoveAll_
+{
+    internal class Program
+    {
+       // public delegate bool Predicate<in T>(T value);
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("TV", 900));
+            list.Add(new Product("Mouse", 50));
+            list.Add(new Product("Tablet", 350));
+            list.Add(new Product("HD Case", 50));
+            //Jeito 1
+            list.RemoveAll(p => p.Price < 100);
+            //Jeito 2
+            list.RemoveAll(ProductTest);
+
+            Console.WriteLine(string.Join("\n",list));
+            
+        }
+        public static bool ProductTest(Product p) => p.Price < 100;
+        
+            
+        
+    }
+}
---
+++ c# POO/Seção 14 - Expressões lambda,delegates,LINQ/PredicateExercicios/PredicateExercicios/Program.cs
@@ -0,0 +1,33 @@
+﻿using PredicateExercicios.Entites;
+using System.Globalization;
+
+namespace PredicateExercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex1
+            List<int> list = new List<int>() { 15, 8, 99, 45, 120, 3 };
+           bool result = list.Exists(x => x > 100);
+            Console.WriteLine(result);
+            //Ex2 
+            List<string> itens = new List<string>() { "Green Herb", "Shotgun", "Handgun", "Red Herb", "Key" };
+            var filtrados = itens.FindAll(item => item.StartsWith("H"));
+            Console.WriteLine(string.Join("\n",filtrados));
+            //Ex 3
+            List<double> pesos = new List<double>() { 1.5, 0.2, 5.0, 0.8, 3.2 };
+            pesos.RemoveAll(x => x < 1);
+            foreach (var peso in pesos) { Console.WriteLine(peso.ToString("F2",CultureInfo.InvariantCulture)); }
+            //Ex4
+            List<Enemy> enemies = new List<Enemy>();
+            enemies.Add(new Enemy("Zumbi",200));
+            enemies.Add(new Enemy("Zumbi de fogo", -50));
+            enemies.Add(new Enemy("Zumbi Tank", 0));
+            enemies.Add(new Enemy("Zumbi rastejante", 1));
+            var aliveEnemys = enemies.FindAll(x => x.Health > 0);
+            Console.WriteLine(string.Join("\n",aliveEnemys));
+
+        }
+    }
+}
