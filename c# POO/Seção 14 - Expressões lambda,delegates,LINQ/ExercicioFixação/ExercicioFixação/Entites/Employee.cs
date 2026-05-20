512c955a170f028bba43d2ca3fdbcc90f910fa21
LucasSouz89 <lucas.ss7@outlook.com>
2026-05-20 18:38:19 -0300

    Fazendo exercicios do cap 14

---
+++ c# POO/Seção 14 - Expressões lambda,delegates,LINQ/ExercicioFixação/ExercicioFixação/Entites/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixação.Entites
+{
+    class Employee
+    {
+        public string Name { get;private set; }
+
+        public string Email { get;private set; }
+
+        public double Salary { get;private set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
---
+++ c# POO/Seção 14 - Expressões lambda,delegates,LINQ/ExercicioFixação/ExercicioFixação/Program.cs
@@ -0,0 +1,47 @@
+﻿using ExercicioFixação.Entites;
+using System.Globalization;
+using System.Linq;
+
+namespace ExercicioFixação
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            ReadFile(employees);
+            ShowInformations(employees);
+            
+        }public static void ReadFile(List<Employee> employees)
+        {
+            try
+            {
+                Console.Write("Enter full file path: ");
+                string path = @"" + Console.ReadLine();
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Trim('\'').Trim('\"').Split(",");
+                        string name = line[0];
+                        string email = line[1];
+                        double salary = double.Parse(line[2], CultureInfo.InvariantCulture);
+                        employees.Add(new Employee(name, email, salary));
+                    }
+
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+        }
+        public static void ShowInformations(List<Employee> employees)
+        {
+            Console.Write("Enter salary: ");
+            double salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine($"Email of people whose salary is more than {salary.ToString("F2",CultureInfo.InvariantCulture)}: ");
+            Console.WriteLine(string.Join("\n",employees.Where(x=> x.Salary > salary).OrderBy(x=> x.Email).Select(x=> x.Email)));
+            var sum = employees.Where(x => x.Name[0] == 'M').Sum(x => x.Salary);
+            Console.WriteLine($"Sum of salary of people whose name starts with 'M': {sum.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+
+    }
+}
