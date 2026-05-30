17e5b9ea30b6146137973fc1cfad838704934962
Lucas Souza <lucas.ss7@outlook.com>
2026-05-30 00:27:47 -0300

    Jogada especial promocao

--- c# POO/Seção 16 -Projeto Sistema de jogo de xadrez/Xadrez-Console/Xadrez-Console/xadrex/PartidaDeXadrez.cs
+++ c# POO/Seção 16 -Projeto Sistema de jogo de xadrez/Xadrez-Console/Xadrez-Console/xadrex/PartidaDeXadrez.cs
@@ -137,6 +137,19 @@ namespace xadrex
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
 
             }
+            Peca p = Tab.peca(destino);
+            //#Jogada especial promoção
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0)|| p.Cor == Cor.Preta && destino.Linha == 7) 
+                {
+                    p = Tab.RetirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
 
             Xeque = (EstaEmXeque(Adversaria(JogadorAtual))) ? true : false;
             if (TesteXequemate(Adversaria(JogadorAtual))) { Terminada = true; }
@@ -145,7 +158,7 @@ namespace xadrex
                 Turno++;
                 MudaJogador();
             }
-            Peca p = Tab.peca(destino);
+            
             //#Jogadaespecial En passant
             if(p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
             {
